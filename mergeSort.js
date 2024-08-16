569c6489ebabc4d8aa94fd788d971cf30ba12ec2
Lav <killerboylav4904@gmail.com>
2024-08-16 13:24:00 +0530

    Merge sort solved

---
+++ mergeSort.js
@@ -0,0 +1,23 @@
+//Merge Sort
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  let leftArr = arr.slice(0, mid);
+  let rightArr = arr.slice(mid);
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+  let sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])); //[ 0, 1, 1, 2, 3, 5, 8, 13 ]
